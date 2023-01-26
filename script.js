c37087ca0ba6b0de02abecc9ad7e7e3d3e16c4a2
Karine C <karine.cordeiro1998@hotmail.com>
2023-01-26 15:07:36 -0300

    adicionando modal em botão de enviar na página contato

---
+++ script.js
@@ -0,0 +1,19 @@
+// Variáveis
+var btnAbreModal = document.querySelector('#abrirModal');
+var modal = document.querySelector('.modalContato');
+var modalBody = document.querySelector('.modalContato-body');
+var modalOverlay = document.querySelector('.modalContato-overlay');
+
+
+// Quando abrir a dialog...
+btnAbreModal.addEventListener('click', function() {
+    modal.classList.add('modalContato--aberto');
+});
+
+function fechandoModal() {
+    document.activeElement.blur();
+    modal.classList.remove('modalContato--aberto');     
+}
+
+// Listeners
+document.querySelector('.modalContato-fechar').addEventListener('click', fechandoModal);
