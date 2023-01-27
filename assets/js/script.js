f1ba557056447ec5a7bfded1468427fde11e9378
Karine C <karine.cordeiro1998@hotmail.com>
2023-01-27 14:33:59 -0300

    inserindo fechamento pela tecla esc e usando overlay

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,26 @@ var modalBody = document.querySelector('.modalContato-body');
 var modalOverlay = document.querySelector('.modalContato-overlay');
 
 
-// Quando abrir a dialog...
+// Quando abrir a modal...
 btnAbreModal.addEventListener('click', function() {
     modal.classList.add('modalContato--aberto');
 });
 
 function fechandoModal() {
     document.activeElement.blur();
-    modal.classList.remove('modalContato--aberto');    
+    modal.classList.remove('modalContato--aberto');  
+    btnAbreModal.focus();
+    
 }
 
 // Listeners
 document.querySelector('.modalContato-fechar').addEventListener('click', fechandoModal);
+
+modalOverlay.addEventListener('click', fechandoModal); //fechar clicando fora
+
+// fechar usando esc
+document.addEventListener('keyup', function(evento) {
+    if (evento.key === 'Escape') {
+        fechandoModal()
+    }
+})
